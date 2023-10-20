354d85ca359629ecc4320a5aa50fbc754e4c5554
TrangVuong2810 <125451980+TrangVuong2810@users.noreply.github.com>
2023-10-20 23:57:36 +0700

    'updateTrie'

--- src/main/java/base/Trie.java
+++ src/main/java/base/Trie.java
@@ -27,21 +27,6 @@ public class Trie {
         temp.setEndOfWord(true);
     }
 
-//    public boolean find(String word) {
-//        TrieNode temp = root;
-//
-//        for (int i = 0; i < word.length(); ++i) {
-//            TrieNode nextNode;
-//            if ((nextNode = temp.getChildren().get(word.charAt(i))) == null) {
-//                return false;
-//            }
-//            temp = nextNode;
-//        }
-//        return temp.isEndOfWord();
-//    }
-
-
-
     public void insert(String word) {
         TrieNode temp = root;
 
@@ -51,74 +36,4 @@ public class Trie {
         temp.setEndOfWord(true);
     }
 
-//    public void seacher(String word) {
-//        TrieNode temp = root;
-//
-//        for (int i = 0; i < word.length(); ++i) {
-//            TrieNode nextNode;
-//            if ((nextNode = temp.getChildren().get(word.charAt(i))) == null) {
-//                return;
-//            }
-//            temp = nextNode;
-//        }
-//        printAllWordWithPrefix(temp, word);
-//    }
-
-//    public void lookup(String word) {
-//        TrieNode temp = root;
-//
-//        for (int i = 0; i < word.length(); ++i) {
-//            TrieNode nextNode;
-//            if ((nextNode = temp.getChildren().get(word.charAt(i))) == null) {
-//                System.out.println("Word not found in dictionary");
-//                return;
-//            }
-//            temp = nextNode;
-//        }
-//        if (temp.isEndOfWord()) {
-//            System.out.println(temp.getMeaning());
-//        } else {
-//            System.out.println("Word not found in dictionary");
-//        }
-//    }
-
-//    public boolean delete(TrieNode root, String word, int depth) {
-//        if (root == null) {
-//            return false;
-//        }
-//
-//        if (depth == word.length()) {
-//            if (root.isEndOfWord()) {
-//                root.setEndOfWord(false);
-//            }
-//            return root.getChildren().isEmpty();
-//        }
-//
-//        TrieNode nextNode = root.getChildren().get(word.charAt(depth));
-//
-//        if (nextNode == null) {
-//            return false;
-//        }
-//        if (delete(nextNode, word, depth + 1) && !nextNode.isEndOfWord()) {
-//            root.getChildren().remove(word.charAt(depth));
-//            return root.getChildren().isEmpty();
-//        }
-//        return false;
-//    }
-
-//    public void edit(String word, String value) {
-//        TrieNode temp = root;
-//
-//        for (int i = 0; i < word.length(); ++i) {
-//            TrieNode nextNode;
-//            if ((nextNode = temp.getChildren().get(word.charAt(i))) == null) {
-//                System.out.println("Word not found in dictionary");
-//                return;
-//            }
-//            temp = nextNode;
-//        }
-//        if (temp.isEndOfWord()) {
-//            temp.setMeaning(value);
-//        }
-//    }
 }
